67775365cee21fc21a672aa2ca98f35759c718a6
HKGx <19597269+HKGx@users.noreply.github.com>
2021-02-05 11:42:54 +0100

    env: add new environment variables for mongo auth

--- src/env.ts
+++ src/env.ts
@@ -3,22 +3,26 @@ import { config } from "dotenv";
 config();
 
 interface _Env {
-  LAMB_MONGO_URL?: string;
-  LAMB_BOT_TOKEN?: string;
-  LAMB_OWNER_ID?: string;
-  LAMB_PREFIX?: string;
+  LAMB_MONGO_URL: string;
+  LAMB_BOT_TOKEN: string;
+  LAMB_OWNER_ID: string;
+  LAMB_PREFIX: string;
+  LAMB_MONGO_USER: string;
+  LAMB_MONGO_PASSWORD: string;
 }
 
-export type Env = Readonly<Required<_Env>>;
+export type Env = Readonly<_Env>;
 
 export function validatedEnv() {
-  const obj: _Env = {};
-  const props: Array<keyof _Env> = [
+  const obj: Record<string, string> = {};
+  const props = [
     "LAMB_MONGO_URL",
     "LAMB_BOT_TOKEN",
     "LAMB_OWNER_ID",
     "LAMB_PREFIX",
-  ];
+    "LAMB_MONGO_USER",
+    "LAMB_MONGO_PASSWORD",
+  ] as const;
   for (const prop of props) {
     const value = process.env[prop];
     if (value) {
