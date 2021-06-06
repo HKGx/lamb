57c8af5d6b6e671b21eac88131799ada1ac81e82
HKGx <19597269+HKGx@users.noreply.github.com>
2021-06-06 17:24:58 +0200

    models/event.model: add new field `organisators`

--- src/models/event.model.ts
+++ src/models/event.model.ts
@@ -2,10 +2,12 @@ import { Document, model, Schema } from "mongoose";
 
 export interface Event extends Document {
   name: string;
+  organisators: string[];
 }
 
 const eventSchema: Schema = new Schema({
   name: { type: String, required: true },
+  organisators: { type: [String] },
 });
 
 export default model<Event>("event", eventSchema);
