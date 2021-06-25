d3a33540b5fe8a72627c5a9ae4b6cf8bfa51e58a
HKGx <19597269+HKGx@users.noreply.github.com>
2021-06-25 21:13:56 +0200

    math.cmd: add scale argument

--- src/customCommands/math.cmd.ts
+++ src/customCommands/math.cmd.ts
@@ -1,13 +1,14 @@
 import { Command, Context } from "../commands";
+import { Integer } from "../commands/converters/NumberConverter";
 
 const RENDERTEX_LINK = "https://rendertex.hkgdoes.dev";
 
 class MathCommand extends Command {
   name = "math";
-  arguments = [String];
-  async invoke(ctx: Context, math: string) {
+  arguments = [{ cls: Integer, optional: true }, String];
+  async invoke(ctx: Context, scale: number | undefined, math: string) {
     const data = encodeURI(math);
-    ctx.reply(`${RENDERTEX_LINK}/latex/jpg/${data}`);
+    ctx.reply(`${RENDERTEX_LINK}/latex/jpg/${data}?scale=${scale || 1}`);
   }
 }
 
